e9aeacea51f64306751b87506439d3e1962430e3
SLOKnightfall <SLOKnightfall@gmail.com>
2023-09-08 17:03:00 -0700

    -Correcting Tags

--- BattlePetCageMatch.lua
+++ BattlePetCageMatch.lua
@@ -2,7 +2,6 @@
 --
 --	BattlePetCageMatch
 --	Author: SLOKnightfall
-
 --	BattlePetCageMatch: Scans bags and puts icons on the Pet Journal for any pet that is currently caged
 --
 --	///////////////////////////////////////////////////////////////////////////////////////////
